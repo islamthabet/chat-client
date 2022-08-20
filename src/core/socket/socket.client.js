ea40e724c9a9b5b4eef373562852322572110a82
islamthabet <islamthabet23@gmail.com>
2022-08-20 12:07:53 +0200

    change api

--- src/core/socket/socket.client.js
+++ src/core/socket/socket.client.js
@@ -9,7 +9,8 @@ peer.on('open', (id) => {
   peerId = id;
 });
 
-const socket = io('http://localhost:5000/', {
+// const socket = io('http://localhost:5000/', {
+const socket = io('https://chat-2023.herokuapp.com/', {
   reconnectionDelayMax: 10000,
   auth: {
     token: localStorage.getItem('token'),
